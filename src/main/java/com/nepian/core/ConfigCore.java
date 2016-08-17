7612a21b573f1b785d1ef086233bb28c8504d3ce
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2016-08-17 09:57:06 +0900

    ConfigCore 修正

--- src/main/java/com/nepian/core/ConfigCore.java
+++ src/main/java/com/nepian/core/ConfigCore.java
@@ -1,45 +1,33 @@
 package com.nepian.core;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Map;
 
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import com.nepian.core.utils.FileUtil;
 import com.nepian.core.utils.Util;
 
-public abstract class ConfigCore {
-	protected File configFile;
+public class ConfigCore {
+	protected static final String FOLDER_NAME = "configs";
+	protected static File folder;
+	protected File file;
+	protected YamlConfiguration yaml;
 	protected Map<String, Object> configs;
 	
-	protected abstract void setConfigs();
-	
-	/**
-	 * コンストラクタ
-	 * @param configFile 設定ファイル
-	 */
-	public ConfigCore(File configFile) {
-		this.configFile = configFile;
-		this.configs = Util.newMap();
-		this.setConfigs();
-		this.read();
+	public ConfigCore(JavaPlugin plugin, String name) {
+		folder = FileUtil.loadFolder(plugin.getDataFolder(), FOLDER_NAME);
+		file = FileUtil.loadFile(folder, name);
+		yaml = FileUtil.getYml(file);
+		configs = Util.newMap();
+		read();
 	}
 	
-	/**
-	 * 設定を取得する
-	 * @param key
-	 * @return
-	 */
 	public Object get(String key) {
 		return configs.get(key);
 	}
 	
-	/**
-	 * 設定を更新する
-	 * @param key
-	 * @param value
-	 */
 	public void update(String key, Object value) {
 		if (configs.containsKey(key)) {
 			write(key, value);
@@ -47,48 +35,14 @@ public abstract class ConfigCore {
 		}
 	}
 	
-	/**
-	 * 設定を追加する
-	 * @param key
-	 * @param value
-	 */
-	protected void add(String key, Object value) {
-		configs.put(key, value);
-	}
-	
-	/**
-	 * 設定をファイルから読み込む
-	 */
 	private void read() {
-		YamlConfiguration data = FileUtil.getYml(configFile);
-		
-		for (String key : configs.keySet()) {
-			if (!data.contains(key)) {
-				write(key, configs.get(key));
-			}
+		for (String key : yaml.getKeys(true)) {
+			configs.put(key, yaml.get(key));
 		}
 	}
 	
-	/**
-	 * 設定をファイルへ書き込む
-	 * @param key
-	 * @param value
-	 */
 	private void write(String key, Object value) {
-		YamlConfiguration data = FileUtil.getYml(configFile);
-		data.set(key, value);
-		save(data);
-	}
-	
-	/**
-	 * 設定を保存する
-	 * @param data
-	 */
-	private void save(YamlConfiguration data) {
-		try {
-			data.save(configFile);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		yaml.set(key, value);
+		FileUtil.saveYml(file, yaml);
 	}
 }
