34c02a21953dc05b7063b0cc0076783e1daf550c
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2016-08-21 02:53:19 +0900

    onEnable() で メッセージを表示

--- src/main/java/com/nepian/core/Core.java
+++ src/main/java/com/nepian/core/Core.java
@@ -3,4 +3,18 @@ package com.nepian.core;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		enableMessage();
+	}
+	
+	private void enableMessage() {
+		String v = this.getDescription().getVersion();
+		Messenger mes = new Messenger(this);
+		
+		mes.log("&d----------------------------------------------");
+		mes.log("&d Enabling Core version " + v );
+		mes.log("&d----------------------------------------------");
+	}
 }
